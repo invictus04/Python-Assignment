8a3ac4872200141fcb9bc62a5e3aacd24a95f46b
Rajvardhan Singh <97658238+invictus04@users.noreply.github.com>
2023-03-24 20:26:45 +0530

    Add files via upload

---
+++ readlines.py
@@ -0,0 +1,8 @@
+file = open("demofile.txt", 'r')
+Lines = file.readlines()
+
+count = 0
+# Strips the newline character
+for line in Lines:
+    count += 1
+    print("Line{}: {}".format(count, line.strip()))
